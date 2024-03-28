c7bc55114495c5c5e47e7c90d3676b59e14be167
EskildVeren <eskild.overen@gmail.com>
2024-03-29 00:00:29 +0100

    implement crossing out todos

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,33 +1,41 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import "./style.css";
+
+type todoDay = {
+  date: string;
+  items: string[];
+};
+
+const todos: todoDay = {
+  date: "today",
+  items: ["Vask klær", "Ring farmor", "Fullfør inlevering", "Mat katten"],
+};
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [strikedItems, setStrikedItems] = useState([""]);
+
+  const strikeItem = (todo: string) => {
+    if (strikedItems.includes(todo)) {
+      setStrikedItems(strikedItems.filter((item) => item != todo));
+    } else {
+      setStrikedItems([...strikedItems, todo]);
+    }
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Todo Today</h1>
+      {todos.items.map((todo, index) => {
+        return (
+          <p
+            key={index}
+            onClick={() => strikeItem(todo)}
+            className={strikedItems.includes(todo) ? "striked" : ""}
+          >
+            {todo}
+          </p>
+        );
+      })}
     </>
   );
 }
